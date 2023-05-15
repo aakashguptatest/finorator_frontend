fe72ea6539e5d9bd2cc6525a89aa67af764b869d
AdityaTiwari03 <aadi.tiwari0208@gmail.com>
2023-05-15 20:45:05 +0530

    added everything except Invest, and reports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import Homepage from './Comp/Homepage';
+import Aboutus from "./Comp/Aboutus";
+import SliderAffiliations from './Comp/Affiliations';
+import BecomePartner from './Comp/BecomePartner';
+import Contactus from './Comp/Contactus';
+import './App.css';
+import Downloads from './Comp/Downloads';
+import MFWatchlist from './Comp/MFWatchlist';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Homepage />
-          </div>
+      <Router>
+        <Routes> 
+        <Route path="/" element={<Homepage />}></Route>
+          <Route path='/partner' element={<BecomePartner />}></Route>
+      </Routes> 
+      </Router>
+ </div>
   );
 }
 
 export default App;
+
