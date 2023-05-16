4a61856b18b79010990af6279599715f4c0c37ff
AdityaTiwari03 <aadi.tiwari0208@gmail.com>
2023-05-16 13:02:35 +0530

    finalised

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import Homepage from './Comp/Homepage';
-import Aboutus from "./Comp/Aboutus";
-import SliderAffiliations from './Comp/Affiliations';
-import BecomePartner from './Comp/BecomePartner';
-import Contactus from './Comp/Contactus';
-import './App.css';
-import Downloads from './Comp/Downloads';
-import MFWatchlist from './Comp/MFWatchlist';
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from "./Comp/Main";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes> 
-        <Route path="/" element={<Homepage />}></Route>
-          <Route path='/partner' element={<BecomePartner />}></Route>
-      </Routes> 
-      </Router>
+      <Main/>
  </div>
   );
 }
---
+++ src/Comp/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Homepage from './Homepage';
+import BecomePartner from './BecomePartner';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+export default function Main() {
+  return (
+    <div>
+        <Router>
+        <Routes> 
+        <Route path="/" element={<Homepage />}></Route>
+          <Route path='/partner' element={<BecomePartner />}></Route>
+      </Routes> 
+      </Router>
+    </div>
+  )
+}
