5da54c2b3a0a790a17297ff236bd002fae9e2f60
Aryan Tiwari <aryantiwari@Aryans-MacBook-Air.local>
2023-05-17 17:27:56 +0530

    navbar added

---
+++ src/Comp/NavBar/Navbar.js
@@ -0,0 +1,41 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+			<h3>FINORATOR</h3>
+			<nav ref={navRef}>
+                <a href="/#">Solutions</a>
+                <a href="/#">About Us</a>
+				<a href="/#">Become a Partner</a>
+				<a href="/#">Resources</a>
+				<a href="/#">About me</a>
+                
+
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
+
+export default Navbar;
+
