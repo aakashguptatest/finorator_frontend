61b7dc53e49d953754e02e0514281442de78a27a
AdityaTiwari03 <aadi.tiwari0208@gmail.com>
2023-05-22 13:12:04 +0530

    login Page

--- src/Comp/NavBar/Navbar.js
+++ src/Comp/NavBar/Navbar.js
@@ -1,26 +1,45 @@
-import { Stack } from "@mui/material";
+import { Stack, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const Navbar = () => (
   <Stack
     direction="row"
     alignItems="center"
-    p={2}
+    p={1.8}
     sx={{
       position: "sticky",
       background: "gray",
       top: 0,
       zIndex: 10000,
-      justifyContent: "space-around",
+      justifyContent: "space-between", // Modified to align items to the left and right
     }}
   >
+    <NavLinks /> {/* Moved the navigation links to a separate component */}
+    <AuthLinks /> {/* Added a component for login/register buttons */}
+  </Stack>
+);
+
+const NavLinks = () => (
+  <Stack direction="row" spacing={2}>
     <NavLink to="/" label="Homepage" />
     <NavLink to="/partner" label="Become Partner" />
     <NavLink to="/aboutus" label="About Us" />
     <NavLink to="/contactus" label="Contact Us" />
-    <NavLink to="/blogs" label="Blogs" />
-    <NavLink to="/landing" label="Mutual Funds" />
-    
+  </Stack>
+);
+
+const AuthLinks = () => (
+  <Stack direction="row" spacing={2} alignItems="center">
+    {/* Add your login and register buttons */}
+    <Button
+    id = "loginbutton"
+      variant="contained"
+      component={Link}
+      to="/login"
+      color="warning"
+    >
+      Login/ Register
+    </Button>
   </Stack>
 );
 
@@ -30,14 +49,14 @@ const NavLink = ({ to, label }) => (
     style={{
       display: "flex",
       alignItems: "center",
-      width: 100,
-      padding: "0.5rem",
+      width:150,
+      padding: "0.1rem",
       color: "white",
       textDecoration: "none",
       transition: "background-color 0.3s ease",
       borderRadius: "5px",
       "&:hover": {
-        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        backgroundColor: "white",
       },
     }}
   >
