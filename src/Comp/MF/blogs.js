95d03f78b0cab0326fa7a75524334a8f95d46a6b
Aditya062003 <adityavinay@iitbhilai.ac.in>
2023-05-22 20:13:36 +0530

    Added Sidebar to portfolio and blogs page

--- src/Comp/MF/blogs.js
+++ src/Comp/MF/blogs.js
@@ -1,24 +1,26 @@
-import React, { useEffect, useState, useRef } from 'react';
-import axios from 'axios';
-import { SearchIcon } from '@heroicons/react/outline';
-import Sidebar from '../Sidebar/Sidebar';
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { SearchIcon } from "@heroicons/react/outline";
+import Sidebar from "../Sidebar/Sidebar";
+
 const Blogs = () => {
   const [news, setNews] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const searchInputRef = useRef(null);
 
   useEffect(() => {
     const fetchData = async () => {
       const options = {
-        method: 'GET',
-        url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete',
+        method: "GET",
+        url: "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete",
         params: {
-          q: 'tesla',
-          region: 'US',
+          q: "tesla",
+          region: "US",
         },
         headers: {
-          'X-RapidAPI-Key': '4f30deea61mshfb6fbc84ac17f2ap102886jsna8ecb6e78c28',
-          'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+          "X-RapidAPI-Key":
+            "4f30deea61mshfb6fbc84ac17f2ap102886jsna8ecb6e78c28",
+          "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
         },
       };
 
@@ -48,47 +50,52 @@ const Blogs = () => {
   };
 
   const blurSearchInput = () => {
-    if (searchTerm.trim() === '') {
+    if (searchTerm.trim() === "") {
       searchInputRef.current.blur();
     }
   };
 
   return (
-    <div className="container bg-white min-h-screen mb-4 mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Latest News</h1>
-      <div className="relative">
-        <input
-          type="text"
-          placeholder="Search by topic or keyword"
-          value={searchTerm}
-          onChange={handleSearch}
-          ref={searchInputRef}
-          className="w-full p-2 pr-10 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          onFocus={focusSearchInput}
-          onBlur={blurSearchInput}
-        />
-        <SearchIcon className="absolute top-3 right-3 text-gray-400 w-5 h-5" />
+    <div className="flex">
+      <div className="w-1/6 z-10">
+        <Sidebar />
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        {filteredNews.map((article) => (
-          <div key={article.uuid} className="bg-white p-4 rounded shadow">
-            <img
-              src={article.thumbnail.resolutions[0].url}
-              alt={article.title}
-              className="mb-2"
-            />
-            <h2 className="text-xl font-bold mb-2">{article.title}</h2>
-            <p className="text-gray-600">{article.publisher}</p>
-            <a
-              href={article.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
-            >
-              Read more
-            </a>
-          </div>
-        ))}
+      <div className="container bg-white min-h-screen mb-4 mx-auto">
+        <h1 className="text-2xl font-bold mb-4">Latest News</h1>
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search by topic or keyword"
+            value={searchTerm}
+            onChange={handleSearch}
+            ref={searchInputRef}
+            className="w-full p-2 pr-10 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            onFocus={focusSearchInput}
+            onBlur={blurSearchInput}
+          />
+          <SearchIcon className="absolute top-3 right-3 text-gray-400 w-5 h-5" />
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          {filteredNews.map((article) => (
+            <div key={article.uuid} className="bg-white p-4 rounded shadow">
+              <img
+                src={article.thumbnail.resolutions[0].url}
+                alt={article.title}
+                className="mb-2"
+              />
+              <h2 className="text-xl font-bold mb-2">{article.title}</h2>
+              <p className="text-gray-600">{article.publisher}</p>
+              <a
+                href={article.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                Read more
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/Comp/profile/Portfolio.js
+++ src/Comp/profile/Portfolio.js
@@ -21,26 +21,28 @@ const LinkItem = ({ link, icon, text, isActive, onClick }) => {
 
   return (
     <>
-    <Sidebar/>
-    <a
-      href={link}
-      className={`flex items-center px-2 py-1 ${
-        isActive ? " border-b-2 text-blue-900 border-gray-800" : "text-gray-500"
-      } ${hovered ? "" : ""}`}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      onClick={onClick}
-      style={{ textDecoration: "none" }}
-    >
-      {icon}
-      <div
-        className={`font-bold ${
-          isActive ? "text-blue-900" : "text-gray-500"
-        } px-2 text-2xl`}
+      <a
+        href={link}
+        className={`flex items-center px-2 py-1 ${
+          isActive
+            ? " border-b-2 text-blue-900 border-gray-800"
+            : "text-gray-500"
+        } ${hovered ? "" : ""}`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onClick={onClick}
+        style={{ textDecoration: "none" }}
       >
-        {text}
-      </div>
-    </a></>
+        {icon}
+        <div
+          className={`font-bold ${
+            isActive ? "text-blue-900" : "text-gray-500"
+          } px-2 text-2xl`}
+        >
+          {text}
+        </div>
+      </a>
+    </>
   );
 };
 
@@ -56,128 +58,135 @@ const Portfolio = () => {
   };
 
   return (
-    <div>
-      <div className="flex border-b-2 border-gray-400 flex-wrap gap-3 mt-4 ml-4">
-        <LinkItem
-          link={"#"}
-          icon={<BsPieChartFill />}
-          text="Portfolio Overview"
-          isActive={activeIndex1 === 0}
-          onClick={() => handleClick1(0)}
-        />
-        <LinkItem
-          link={"#"}
-          icon={<BsGraphUpArrow />}
-          text="Portfolio Overview"
-          isActive={activeIndex1 === 1}
-          onClick={() => handleClick1(1)}
-        />
-        <LinkItem
-          link={"#"}
-          icon={<BsFileEarmarkPdf />}
-          text="Portfolio Overview"
-          isActive={activeIndex1 === 2}
-          onClick={() => handleClick1(2)}
-        />
+    <div className="flex">
+      <div className="w-1/6 z-10">
+        <Sidebar />
       </div>
-      <div className="flex border-b-2 border-gray-400 flex-wrap gap-3 mt-4 ml-4">
-        <LinkItem
-          link={"#"}
-          text="All"
-          isActive={activeIndex2 === 0}
-          onClick={() => handleClick2(0)}
-        />
-        <LinkItem
-          link={"#"}
-          text="Mutual Funds"
-          isActive={activeIndex2 === 1}
-          onClick={() => handleClick2(1)}
-        />
-      </div>
-      <div className="flex justify-center m-6 gap-8 flex-wrap">
-        <div class="grid mt-4 gap-6">
-          <div class="px-10 py-2 border border-gray-300 rounded bg-white text-gray-700 shadow-lg">
-            <div className="text-center text-2xl font-bold">Investments</div>
-          </div>
-          <div class="px-6  border border-gray-300 rounded bg-white text-gray-700 shadow-lg">
-            <div className="text-center text-2xl font-bold">Current Value</div>
-          </div>
-          <div class="px-6 border border-gray-300 rounded bg-white text-gray-700 shadow-lg">
-            <div className="text-center text-2xl font-bold">Unrealized </div>
-            <div className="text-center text-2xl font-bold">Gain/Loss</div>
-          </div>
+      <div className="min-h-screen">
+        <div className="flex border-b-2 border-gray-400 flex-wrap gap-3 mt-4 ml-4">
+          <LinkItem
+            link={"#"}
+            icon={<BsPieChartFill />}
+            text="Portfolio Overview"
+            isActive={activeIndex1 === 0}
+            onClick={() => handleClick1(0)}
+          />
+          <LinkItem
+            link={"#"}
+            icon={<BsGraphUpArrow />}
+            text="Portfolio Overview"
+            isActive={activeIndex1 === 1}
+            onClick={() => handleClick1(1)}
+          />
+          <LinkItem
+            link={"#"}
+            icon={<BsFileEarmarkPdf />}
+            text="Portfolio Overview"
+            isActive={activeIndex1 === 2}
+            onClick={() => handleClick1(2)}
+          />
         </div>
+        <div className="flex border-b-2 border-gray-400 flex-wrap gap-3 mt-4 ml-4">
+          <LinkItem
+            link={"#"}
+            text="All"
+            isActive={activeIndex2 === 0}
+            onClick={() => handleClick2(0)}
+          />
+          <LinkItem
+            link={"#"}
+            text="Mutual Funds"
+            isActive={activeIndex2 === 1}
+            onClick={() => handleClick2(1)}
+          />
+        </div>
+        <div className="flex justify-center m-6 gap-8 flex-wrap">
+          <div class="grid mt-4 gap-6">
+            <div class="px-10 py-2 border border-gray-300 rounded bg-white text-gray-700 shadow-lg">
+              <div className="text-center text-2xl font-bold">Investments</div>
+            </div>
+            <div class="px-6  border border-gray-300 rounded bg-white text-gray-700 shadow-lg">
+              <div className="text-center text-2xl font-bold">
+                Current Value
+              </div>
+            </div>
+            <div class="px-6 border border-gray-300 rounded bg-white text-gray-700 shadow-lg">
+              <div className="text-center text-2xl font-bold">Unrealized </div>
+              <div className="text-center text-2xl font-bold">Gain/Loss</div>
+            </div>
+          </div>
 
-        <Chart
-          type="donut"
-          width={1000}
-          height={450}
-          series={[100]} //a.percentage of each fund
-          options={{
-            dataLabels: {
-              enabled: true,
-            },
-            colors: ["#4472C4"],
-            legend: {
-              show: true,
-              position: "right",
-              itemMargin: {
-                horizontal: 5,
-                vertical: 200,
+          <Chart
+            type="donut"
+            width={1000}
+            height={450}
+            series={[100]} //a.percentage of each fund
+            options={{
+              dataLabels: {
+                enabled: true,
               },
-              onItemClick: {
-                toggleDataSeries: true,
+              colors: ["#4472C4"],
+              legend: {
+                show: true,
+                position: "right",
+                itemMargin: {
+                  horizontal: 5,
+                  vertical: 200,
+                },
+                onItemClick: {
+                  toggleDataSeries: true,
+                },
+                onItemHover: {
+                  highlightDataSeries: true,
+                },
+                fontSize: "14px",
+                markers: {
+                  width: 14,
+                  height: 14,
+                  strokeWidth: 0,
+                  strokeColor: "#375DA1",
+                  fillColors: undefined,
+                  radius: 16,
+                  customHTML: undefined,
+                  onClick: undefined,
+                  offsetX: 0,
+                  offsetY: 0,
+                },
+                horizontalAlign: "center",
               },
-              onItemHover: {
-                highlightDataSeries: true,
-              },
-              fontSize: "14px",
-              markers: {
-                width: 14,
-                height: 14,
-                strokeWidth: 0,
-                strokeColor: "#375DA1",
-                fillColors: undefined,
-                radius: 16,
-                customHTML: undefined,
-                onClick: undefined,
-                offsetX: 0,
-                offsetY: 0,
-              },
-              horizontalAlign: "center",
-            },
-            responsive: [
-              {
-                breakpoint: 480,
-                options: {
-                  chart: {
-                    width: 300,
-                  },
-                  legend: {
-                    position: "bottom",
-                    horizontalAlign: "left",
+              responsive: [
+                {
+                  breakpoint: 480,
+                  options: {
+                    chart: {
+                      width: 300,
+                    },
+                    legend: {
+                      position: "bottom",
+                      horizontalAlign: "left",
+                    },
                   },
                 },
-              },
-            ],
-            labels: [
-              "a.SBI Equity Fund - Regular Plan - Gr", //a.mutual fund name
-            ],
-            plotOptions: {
-              pie: {
-                donut: {
-                  labels: {
-                    show: true,
-                    total: {
+              ],
+              labels: [
+                "a.SBI Equity Fund - Regular Plan - Gr", //a.mutual fund name
+              ],
+              plotOptions: {
+                pie: {
+                  donut: {
+                    labels: {
                       show: true,
-                      fontSize: 12,
+                      total: {
+                        show: true,
+                        fontSize: 12,
+                      },
                     },
                   },
                 },
               },
-            },
-          }}
-        />
+            }}
+          />
+        </div>
       </div>
     </div>
   );
