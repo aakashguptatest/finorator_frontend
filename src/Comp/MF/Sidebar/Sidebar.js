0644871e308d45a2eee87ec02a5bb70c87c76362
AdityaTiwari03 <aadi.tiwari0208@gmail.com>
2023-05-29 17:47:21 +0530

    Landing Page and MF Sidebar

--- src/Comp/MF/Sidebar/Sidebar.js
+++ src/Comp/MF/Sidebar/Sidebar.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
-import { BsDownload } from "react-icons/bs";
 import { FaUserCircle, FaShoppingCart } from "react-icons/fa";
 import { MdOutlineMessage } from "react-icons/md";
-import { BsFillBookmarkCheckFill } from "react-icons/bs";
 import { AiFillPieChart } from "react-icons/ai";
-import Dropdown from 'react-dropdown';
+import Dropdown from "react-bootstrap/Dropdown";
 
-import { useNavigate } from "react-router";
 
 import "./Sidebar.css";
 
 const menuItems = [
   {
-    text: "Portfolio",
-    icon: <BsDownload />,
-    lk: "/portfolio",
+    text: "Profile",
+    icon: <FaUserCircle />,
+    dropdownItems: [
+      { text: "Portfolio", lk: "/portfolio" },
+      { text: "Report", lk: "/report" },
+      { text: "Transactions", lk: "/transaction" },
+      { text: "Watchlist", lk: "/mf/watchlist" }
+
+    ],
   },
   {
     text: "Investment",
@@ -36,53 +39,32 @@ const menuItems = [
     icon: <FaShoppingCart />,
     lk: "/help",
   },
+  {
+    text: "Settings",
+    icon: <FaShoppingCart />,
+    dropdownItems: [
+      { text: "Bank Mandate Details", lk: "/demat" },
+      { text: "Basic Information", lk: "/basic" },
+      { text: "Login/Change Password", lk: "/demat" },
+      { text: "Demat/Account Closure", lk: "/demat" }
+
+    ],
+  },
+  {
+    text: "Research/Blogs",
+    icon: <FaShoppingCart />,
+    dropdownItems: [
+      { text: "Research", lk: "/research" },
+      { text: "Blogs", lk: "/blogs" },
+   
+    ],
+  }
+
 ];
+  
 
 const Sidebar = () => {
-  const nav = useNavigate();
   const [isExpanded, setExpandedState] = useState(false);
-  const optionsSettings = [
-    'Bank Mandate Detials', 'Basic Information', 'Login/Change Password', 'Demat/Account Closure'
-  ];
-  const optionsBlogs = [
-    'Research', 'Blogs'
-  ];
-  const optionsProfile = [
-    'Profile', 'Portfolio', "Transaction", "Reports", "Watchlist"
-  ];
-  const defaultOptionProfile = "Profile";
-  const defaultOptionSettings = "Settings";
-  const defaultOptionResearch = "Research/Blogs"
-  const handleDropdownSelectSettings = (selectedOption) => {
-    if (selectedOption.value == "Basic Information"){
-      nav(`/basic`)
-    }
-    else{
-      nav(`/demat`)}
-  };
-  const handleDropdownSelectResearchc = (selectedOption) => {
-    if (selectedOption.value == "Research"){
-      nav(`/research`)
-    }
-    else{
-      nav(`/mf/blogs`)}
-  };
-  const handleDropdownSelectProfile = (selectedOption) => {
-    if (selectedOption.value == "Profile"){
-      nav(`/profile`)
-    }
-    if (selectedOption.value == "Portfolio"){
-      nav(`/portfolio`)
-    }
-    if (selectedOption.value == "Transaction"){
-      nav(`/transaction`)
-    }
-    if (selectedOption.value == "Reports"){
-      nav(`/reports`)
-    }
-    if (selectedOption.value == "Watchlist"){
-      nav(`/mf/watchlist`)
-    }  };
  
   const toggleSidebar = () => {
     setExpandedState(!isExpanded);
@@ -104,27 +86,28 @@ const Sidebar = () => {
           </button>
         </div>
         <div className="sidebar-menu">
-        <Dropdown
-            options={optionsProfile}
-            onChange={handleDropdownSelectProfile}
-            value={defaultOptionProfile}
-            placeholder="Select an option"
-          />
-
-        <Dropdown
-            options={optionsSettings}
-            onChange={handleDropdownSelectSettings}
-            value={defaultOptionSettings}
-            placeholder="Select an option"
-          />
-          <Dropdown
-            options={optionsBlogs}
-            onChange={handleDropdownSelectResearchc}
-            value={defaultOptionResearch}
-            placeholder="Select an option"
-          />
-
-          {menuItems.map(({ text, icon, lk }, index) => (
+        {menuItems.map(({ text, icon, lk, dropdownItems }, index) => (
+        <React.Fragment key={index}>
+          {dropdownItems ? (
+            <Dropdown style={{marginLeft: "0"}}>
+              <Dropdown.Toggle
+                variant="tertiary"
+                style={{backgroundColor: "white", padding: "0", borderColor: "white", fontSize: "larger"}}
+                id="dropdown-button"
+                className={`menu-item ${isExpanded ? "" : "collapsed"}`}
+              >
+                {icon}
+                {isExpanded && <span style={{fontSize: "large"}}>{text}</span>}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                {dropdownItems.map((item, index) => (
+                  <Dropdown.Item key={index} href={item.lk}>
+                    {item.text}
+                  </Dropdown.Item>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          ) : (
             <a
               key={index}
               className={`menu-item ${isExpanded ? "" : "collapsed"}`}
@@ -133,9 +116,10 @@ const Sidebar = () => {
               {icon}
               {isExpanded && <span>{text}</span>}
             </a>
-          ))}
-        </div>
-      </div>
+          )}
+        </React.Fragment>
+      ))}
+        </div>      </div>
       <div className="sidebar-footer">
         {isExpanded && (
           <div className="sidebar-details">
