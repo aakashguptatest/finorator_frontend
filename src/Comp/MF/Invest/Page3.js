2e72a58a89c0cc4a885807760679f9fec55cc5bc
AdityaTiwari03 <aadi.tiwari0208@gmail.com>
2023-06-05 19:27:32 +0530

    changed css

--- src/Comp/MF/Invest/Page3.js
+++ src/Comp/MF/Invest/Page3.js
@@ -8,27 +8,11 @@ const Page3 = () => {
   console.log(location.state.results);
   var results = location.state.results;
   console.log(results);
-  const [checkbox1, setCheckbox1] = useState(false);
-  const [checkbox2, setCheckbox2] = useState(false);
   const [selectedOption, setSelectedOption] = useState("Switch in/out");
   const [formattedDate, setFormattedDate] = useState("");
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
-  };
-
-  const handleCheckbox1Change = () => {
-    setCheckbox1(!checkbox1);
-  };
-
-  const handleCheckbox2Change = () => {
-    setCheckbox2(!checkbox2);
-  };
-
-  const handleGoButtonClick = () => {
-    // Perform some action with the checkbox values
-    console.log("Checkbox 1:", checkbox1);
-    console.log("Checkbox 2:", checkbox2);
   };
   useEffect(() => {
     const currentDate = new Date();
@@ -50,18 +34,40 @@ const Page3 = () => {
 
   return (
     <div className="container">
-      <div className="scheme">
-        <h2>Scheme</h2>
+      
+        <p style={{backgroundColor: "lightblue", fontSize: "3em", padding: "0.2em", color: "blue", fontWeight: "bold"}}>Scheme</p>
+        <div className="scheme">
         <div className="scheme-content">
-          <p id="scheme">
-            {results.scheme_name} <br /> <br /> <br /> Folio Number: 12345
-            Amount: 12345 Folio Number: 12345
-          </p>
-          <p>Amount: 0, Units: 0.00</p>
+        <div style={{width: "100%"}}>
+  <div style={{fontSize: "1.4em"}}>
+    category: Large and Mid Cap Fund <br />
+    fundHouse:SBI Mutual Fund<br/>
+    fundSize: 9077 Cr <br />
+    Express Ratio: 1.04%
+  </div>
+</div>
+   
+          <div>
+           
 
-          <div style={{ display: "block" }}>
-            <label htmlFor="options">Select an option:</label>
-            <select
+            <div className="nav-rating">
+              <p style={{ fontSize: "2em" }}>
+                NAV: <b>1234</b> ({formattedDate})
+                <div>
+                  Crisil Rank -
+                  <div className="rating">
+                  <StarRating rating={2}/></div>
+                  Risk: Very High
+                 
+                </div>
+               </p>
+
+            </div>
+          </div>
+        </div>
+        <Chart />
+<br/>
+         <select
               id="options"
               value={selectedOption}
               onChange={handleOptionChange}
@@ -70,60 +76,6 @@ const Page3 = () => {
               <option value="Redemption">Redemption</option>
               <option value="Purchase">Purchase</option>
             </select>
-
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <label style={{ marginRight: "10px", fontSize: "x-large" }}>
-                <input
-                  type="checkbox"
-                  checked={checkbox1}
-                  onChange={handleCheckbox1Change}
-                />
-                Regular
-              </label>
-              <label style={{ marginRight: "10px", fontSize: "x-large" }}>
-                <input
-                  type="checkbox"
-                  checked={checkbox2}
-                  onChange={handleCheckbox2Change}
-                />
-                Direct
-              </label>
-              <button className="go-button" onClick={handleGoButtonClick}>
-                Go
-              </button>
-            </div>
-            <div className="nav-rating">
-              <p style={{ fontSize: "1.5em" }}>
-                NAV: <b>1234</b> ({formattedDate})
-                <div>
-                  Crisil Rank -{" "}
-                  <span style={{ display: "flex" }}>
-                    <StarRating rating={2} />
-                  </span>
-                </div>
-                <span>Risk: Very High</span>
-              </p>
-
-              <div class="cont">
-                <div>
-                  <p>
-                    category: Large and Mid Cap Fund <br />
-                    fundHouse:SBI Mutual Fund
-                  </p>
-                  <p>
-                    fundSize: 9077 Cr <br />
-                    Express Ratio: 1.04%
-                  </p>{" "}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <Chart />
-
-        <p>
-          <b>Total Amount: 0</b>
-        </p>
       </div>
       <br />
       <div className="division">
