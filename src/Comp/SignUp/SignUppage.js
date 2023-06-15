f89e1c20fcd24e362da143b1de9f5e368891de8a
AdityaTiwari03 <aadi.tiwari0208@gmail.com>
2023-06-15 18:06:23 +0530

    login  and signup css

--- src/Comp/SignUp/SignUppage.js
+++ src/Comp/SignUp/SignUppage.js
@@ -76,13 +76,13 @@ function SignUp() {
             <div className="d-flex flex-row align-items-center justify-content-center">
               <p className="lead fw-normal mb-0 me-3">Sign in with</p>
               <div className="flex mx-3 justify-between">
-                <div className="mr-6 hover:scale-[3] cursor-pointer scale-[2.5]">
+                <div className="mr-10 scale-[3.5] hover:scale-[4] cursor-pointer ">
                   <AiFillGoogleCircle />
                 </div>
-                <div className="mr-6 hover:scale-[3] cursor-pointer scale-[2.5]">
+                <div className="mr-10 scale-[3.5] hover:scale-[4] cursor-pointer ">
                   <AiFillLinkedin />
                 </div>
-                <div className="hover:scale-[3] cursor-pointer scale-[2.5]">
+                <div className="scale-[3.5] hover:scale-[4] cursor-pointer ">
                   <AiFillTwitterCircle />
                 </div>
               </div>
@@ -90,68 +90,71 @@ function SignUp() {
           </div>
 
           <div className="divider d-flex align-items-center my-4">
-            <p className="text-center fw-bold mx-3 mb-0">Or</p>
+            <p className="text-center fw-bold mx-3 mb-0 h3">Or</p>
           </div>
 
-          <MDBInput
-            wrapperClass="mb-4"
-            label="Username"
-            id="formControlLg"
-            type="username"
-            size="lg"
-            onChange={handleUsernameChange}
-          />
-          <MDBInput
-            wrapperClass="mb-2"
-            label="First Name"
-            id="formControlLg"
-            type="email"
-            size="lg"
-            onChange={handleFirstnameChange}
-          />
-          <MDBInput
-            wrapperClass="mb-2"
-            label="Second Name"
-            id="formControlLg"
-            type="email"
-            size="lg"
-            onChange={handleLastnameChange}
-          />
-          <MDBInput
-            wrapperClass="mb-4"
-            label="Email address"
-            id="formControlLg"
-            type="email"
-            size="lg"
-            onChange={handleEmailChange}
-          />
-          <MDBInput
-            wrapperClass="mb-4"
-            label="Password"
-            id="formControlLg"
-            type="password"
-            size="lg"
-            onChange={handlePasswordChange}
-          />
+<label htmlFor="formControlLg" className="mb-2">Username</label>
+<MDBInput
+  wrapperClass="mb-4"
+  id="formControlLg"
+  type="username"
+  size="lg"
+  onChange={handleUsernameChange}
+/>
+<label htmlFor="formControlLg" className="mb-2">First Name</label>
+<MDBInput
+  wrapperClass="mb-2"
+  id="formControlLg"
+  type="email"
+  size="lg"
+  onChange={handleFirstnameChange}
+/>
 
-          <div className="d-flex justify-content-between mb-4">
-            <MDBCheckbox
-              name="flexCheck"
-              value=""
-              id="flexCheckDefault"
-              label="Remember me"
-            />
-            <a href="!#">Forgot password?</a>
-          </div>
+<label htmlFor="formControlLg" className="mb-2">Last Name</label>
+<MDBInput
+  wrapperClass="mb-2"
+  id="formControlLg"
+  type="email"
+  size="lg"
+  onChange={handleLastnameChange}
+/>
 
-          <div className="text-center text-md-start mt-4 pt-2">
-            <MDBBtn className="mb-0 px-5" size="lg" onClick={sendLoginRequest}>
+<label htmlFor="formControlLg" className="mb-2">Email Address</label>
+<MDBInput
+  wrapperClass="mb-4"
+  id="formControlLg"
+  type="email"
+  size="lg"
+  onChange={handleEmailChange}
+/>
+
+<label htmlFor="formControlLg" className="mb-2">Password</label>
+<MDBInput
+  wrapperClass="mb-4"
+  id="formControlLg"
+  type="password"
+  size="lg"
+  onChange={handlePasswordChange}
+/>
+
+<div className="inline-flex items-center align-middle mt-0"> 
+  <label htmlFor="flexCheckDefault" className="mr-3">Remember me</label> 
+  <MDBCheckbox
+    className="h-6 w-6 ml-4"
+    name="flexCheck"
+    value=""
+    id="flexCheckDefault"
+  />
+</div>
+
+<div className="flex">
+              <MDBBtn className="text-left mb-0 px-5" size="lg" onClick={sendLoginRequest}>
               Login
             </MDBBtn>
-            <p className="small fw-bold mt-2 pt-1 mb-2">
-              Don't have an account?{" "}
-              <a href="../signup" className="link-danger">
-                Register
+            <p className="text-right large fw-bold mt-2 pt-1 mb-2">
+              Aleady have an account?{" "}
+              <a href="/login" className="link-danger">
+                Login
               </a>
             </p>
           </div>
